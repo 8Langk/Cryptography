7606a628fc11924f876ce89b1d3566d203d44735
이성민 <dltjdals7816@naver.com>
2024-09-09 03:05:01 +0900

    RSA.py

--- asymmentric/RSA.py
+++ asymmentric/RSA.py
@@ -1,9 +1,34 @@
 from Crypto.Util.number import *
 
+class RSA:
 
+    def __init__(self,p,q,e):
+       self.p = p #getprime 512byte
+       self.q = q #getprime 512byte
+       self.e = e
+       self.phi = (p-1)*(q-1)
+       self.d = pow(e,-1,self.phi)
+       self.n = p*q
+       
+    def encrypt(self, m):
+        return pow(m,self.e,self.n)
+
+    def decrypt(self, c):
+        return pow(c,self.d,self.n)
 
 p = getStrongPrime(512)
 q = getStrongPrime(512)
 
+e = 0x10001
 
-print(p,q)
+rsa = RSA(p, q, e)
+
+plaintext = b'LSM{Th1s_1s_my_rsa_c0d3}'
+
+encrypt = rsa.encrypt(bytes_to_long(plaintext))
+print("encrypt : ",long_to_bytes(encrypt))
+
+decrypt = rsa.decrypt(encrypt)
+print("decrypt : ",long_to_bytes(decrypt))
+
+
